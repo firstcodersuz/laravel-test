bd077132f270f5d61780eb6492584cee8d14cdf8
Ruslan Rizayev <rrizayev646@gmail.com>
2023-02-24 22:55:06 +0500

    Application model

---
+++ app/Models/Application.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'subject',
+        'message',
+        'file_url'
+    ];
+
+    public function user(){return BelongsTo(User::class);}
+
+}
