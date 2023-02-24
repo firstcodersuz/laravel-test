2895bae67bb9fc6d776cc8dc0bd5537488883d69
Ruslan Rizayev <rrizayev646@gmail.com>
2023-02-24 22:28:15 +0500

    User and Role models

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'role_id',
         'email',
         'password',
     ];
@@ -41,4 +42,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role(){return $this->belongsTo(Role::class);}
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>"admin",
+            'role_id'=>1,
+            'email'=>"admin@gmail.com",
+            'password'=>Hash::make('ruslan')
+        ]);
+    }
+}
